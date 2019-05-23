efe09ecd4493d7e3b858aae079e20d15d13a6fbf
Joshua Crum <joshua.crum@gmail.com>
2019-05-23 16:06:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,24 +8,31 @@ class CashRegister
     @employee_discount = employee_discount
     @items = []
   end
+  
   def discount
     self.employee_discount
   end
+  
   def items
     @items
   end
+  
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
     quantity.times do
       items << title
     end
   end
+  
   def apply_discount
     if @employee_discount
-      @total = @total * (1 - @employee_discount / 100)
+      @total -= @total * @employee_discount / 100
       "After the discount, the total comes to $#{@total}"
     else
       "There is no discount to apply."
     end
   end
+
+  def void_last_transaction
+    
 end
