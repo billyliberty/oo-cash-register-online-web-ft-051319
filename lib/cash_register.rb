e84012e78ed8537c51d6e5c51b000142ab369efa
Joshua Crum <joshua.crum@gmail.com>
2019-05-23 15:56:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :employee_discount, :price, :item
+
+  def initialize(employee_discount = nil)
+    @total = 0
+    @employee_discount = employee_discount
+    @quantity = []
+  end
+
+  def add_item(price, item, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
+
+  def apply_discount
+    if discount != 0
+
+
+end
