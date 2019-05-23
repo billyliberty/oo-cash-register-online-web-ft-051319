5c818cc91989b8e72f61c8e190c0c72a183df649
Joshua Crum <joshua.crum@gmail.com>
2019-05-23 16:14:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :employee_discount, :items
+  attr_accessor :total, :employee_discount, :items, :price
 
   def initialize (employee_discount = nil)
     @total = 0
@@ -18,6 +18,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
+    @price = price
     self.total += price * quantity
     quantity.times do
       items << title
